b401995ace82e5f48f38e276932bbb47ba326874
bchaerel <brecht.chaerel@gmail.com>
2021-05-11 16:02:55 +0200

    story 1: changed the html + ts files. bug to backend. if fixed: delete the console.log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {NgModule} from '@angular/core';
 import {LoginComponent} from './login/login.component';
 import {HelloWorldComponent} from './hello-world/hello-world.component';
 import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'hello-world', component: HelloWorldComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
