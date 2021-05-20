ce67b9036a28fc1fbd914ceef88ad5fc6f3eb742
pelinyvg <pelin.yuzbasi@gmail.com>
2021-05-20 17:00:00 +0200

    story 34: access to profile, story 2: login (reset)

--- src/app/users/profile-coachee/profile-coachee.component.ts
+++ src/app/users/profile-coachee/profile-coachee.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Coachee} from '../../model/coachee';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CoacheeService} from '../../services/coachee.service';
-import {Observable} from 'rxjs';
+import {AuthenticationService} from '../../authentication/authentication.service';
 
 @Component({
   selector: 'app-profile-coachee',
@@ -13,16 +13,30 @@ export class ProfileCoacheeComponent implements OnInit {
 
   coachee: Coachee;
   id: number;
+  // role = 'Coachee';
 
   constructor(
     private route: ActivatedRoute,
     private service: CoacheeService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+  }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params.id;
-    this.getUser(this.id);
+
+    /*if (this.authenticationService.isAdmin() && this.authenticationService.getUserId() == this.id) {
+      this.role = 'Admin';
+    }*/
+
+    if (this.authenticationService.getUserId() == this.id || this.authenticationService.isAdmin()) {
+      this.getUser(this.id);
+    } else {
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+
+
   }
 
   getUser(id: number): void {
