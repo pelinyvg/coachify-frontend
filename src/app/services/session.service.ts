e7c0ec5d6ecc25f8702cc5f15ed8e0e0a167dabc
bchaerel <brecht.chaerel@gmail.com>
2021-05-26 15:00:58 +0200

    story 14: sessions overview: changed the html layout into upcoming/archive
    extra: changed the topnavbar for coachee so CSS changes when active

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -14,19 +14,19 @@ import {DatePipe} from '@angular/common';
 export class SessionService {
   currentDate: Date = new Date();
 
-  private route = `${environment.backendUrl}/sessions`;
+  private route = environment.backendUrl;
 
   constructor(private http: HttpClient) {
   }
 
   addSession(coachingSession: CoachingSession): Observable<CoachingSession> {
     console.log(coachingSession);
-    return this.http.post<CoachingSession>(this.route, coachingSession);
+    return this.http.post<CoachingSession>(`${this.route}/sessions`, coachingSession);
   }
 
   getSessions(id: number): Observable<CoachingSession[]> {
     console.log(`${this.route}/coachee/${id}`);
-    return this.http.get<CoachingSession[]>(`${this.route}/coachee/${id}`);
+    return this.http.get<CoachingSession[]>(`${this.route}/coachees/${id}/sessions`);
   }
 
   getSessionsUpcoming(id: number): Observable<CoachingSession[]> {
@@ -34,7 +34,7 @@ export class SessionService {
 
     const currentDate = datepipe.transform(this.currentDate, 'yyyy-MM-dd');
     console.log(currentDate);
-    return this.http.get<CoachingSession[]>(`${this.route}/coachee/${id}`)
+    return this.http.get<CoachingSession[]>(`${this.route}/coachees/${id}/sessions`)
       .pipe(
         // tslint:disable-next-line:no-shadowed-variable
         map(response => response.filter(
@@ -49,7 +49,7 @@ export class SessionService {
 
     const currentDate = datepipe.transform(this.currentDate, 'yyyy-MM-dd');
     console.log(currentDate);
-    return this.http.get<CoachingSession[]>(`${this.route}/coachee/${id}`)
+    return this.http.get<CoachingSession[]>(`${this.route}/coachees/${id}/sessions`)
       .pipe(
         // tslint:disable-next-line:no-shadowed-variable
         map(response => response.filter(
