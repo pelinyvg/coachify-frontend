3d264b461a7b754ec7d4316971b93b74252b0cda
ben <benjamin.cabaux@gmail.com>
2021-05-27 16:05:23 +0200

    topicName

--- src/app/services/coach.service.ts
+++ src/app/services/coach.service.ts
@@ -22,6 +22,6 @@ export class CoachService {
   }
 
   getTopics() {
-    return this.http.get<string[]>(this.root + `/topicNames`);
+    return this.http.get<string[]>(this.root + `/coaches/topicNames`);
   }
 }
