6333f92c04018383f2881a3c33d1816035934b4f
ben <benjamin.cabaux@gmail.com>
2021-05-27 11:17:37 +0200

    dropdown for topics in coachs overview

--- src/app/users/overview-coaches/overview-coaches.component.ts
+++ src/app/users/overview-coaches/overview-coaches.component.ts
@@ -1,23 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Coach} from '../../model/coach';
 import {CoachService} from '../../services/coach.service';
 import {Topic} from '../../model/topic';
+import {InitService} from '../../materialize/init.service';
 
 @Component({
   selector: 'app-overview-coaches',
   templateUrl: './overview-coaches.component.html',
   styleUrls: ['./overview-coaches.component.css']
 })
-export class OverviewCoachesComponent implements OnInit {
+export class OverviewCoachesComponent implements OnInit, AfterViewInit {
   coaches: Coach[];
   topicNames: string[];
 
-  constructor(private coachService: CoachService) {
+  constructor(private coachService: CoachService, private initService: InitService) {
   }
 
   ngOnInit(): void {
     this.getCoaches();
     this.getTopics();
+  }
+
+  ngAfterViewInit(): void {
+    this.initService.initDropdowns();
+    this.initService.initSidenav();
   }
 
   getCoaches(): void {
