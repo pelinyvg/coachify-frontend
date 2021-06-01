ecbfdce328a6e20e2af61173d930be13d3f9b28c
Robert Khomiakov <robertkhomiakov@hotmail.com>
2021-06-01 16:40:12 +0200

    story 15

--- src/app/users/coach-navbar-side/coach-navbar-side.component.ts
+++ src/app/users/coach-navbar-side/coach-navbar-side.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../authentication/authentication.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {CoachService} from '../../services/coach.service';
 
 @Component({
@@ -11,8 +11,13 @@ import {CoachService} from '../../services/coach.service';
 export class CoachNavbarSideComponent implements OnInit {
   id: number;
   coachId: number;
+  color:string;
 
-  constructor(private route: ActivatedRoute, private authserv: AuthenticationService, private coachService: CoachService) {
+  constructor(private route: ActivatedRoute,
+              private authserv: AuthenticationService,
+              private coachService: CoachService,
+              private router: Router) {
+    this.color = 'yellow darken-2';
   }
 
   ngOnInit(): void {
@@ -21,6 +26,11 @@ export class CoachNavbarSideComponent implements OnInit {
     // this.coachService.getCoachIdbyCoacheeId(this.id).subscribe(cId => this.coachId = cId);
     this.coachId = this.route.snapshot.params.id;
     console.log('coach id : ' + this.coachId + '/ id : ' + this.id);
+    if (this.router.url.indexOf('/coaches/')  === 0) {
+      this.color = 'teal lighten-3';
+    } else {
+      this.color = 'yellow darken-2';
+    }
   }
 
 }
