3278c210eaaf2d193327d0b4997d9c79d9c71691
ben <benjamin.cabaux@gmail.com>
2021-06-02 11:36:37 +0200

    story 7 : update coach profile, service changes

--- src/app/update-profile-coach/update-profile-coach.component.ts
+++ src/app/update-profile-coach/update-profile-coach.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {CoachService} from '../services/coach.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-update-profile-coach',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-profile-coach.component.css']
 })
 export class UpdateProfileCoachComponent implements OnInit {
+  updateCoachProfileForm = this.formBuilder.group({
+    introduction: ['', [Validators.required]],
+    availability: ['', [Validators.required]]
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private coachService: CoachService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
   }
 
+  get introduction() {
+    return this.updateCoachProfileForm.get('introduction');
+  }
+
+  get availability() {
+    return this.updateCoachProfileForm.get('availability');
+  }
+
+  onSubmit() {
+    this.coachService.changeProfile(this.route.snapshot.params.id, this.updateCoachProfileForm.value).subscribe();
+  }
 }
